472ff18978ffd189bcbb9144a87bcc9a12a3a08d
Han Wang <han.wang.19@ucl.ac.uk>
2026-05-06 12:27:01 +0100

    Added network size extraction and plotting.

--- ExampleUseMSHBM.m
+++ ExampleUseMSHBM.m
@@ -1,4 +1,4 @@
-% define paths 
+%% define paths 
 Paths{1} = '~/Apps/Programming/matlab-proj/PFM_MSHBM_MHVerbGen/res0urces/read_write_cifti'; % this is the path to location containing ft_read/write functions
 Paths{2} = '~/Apps/Utils/CBIG-master'; % this is the path the Thomas Yeo's functions, that have been modified by us
 
@@ -27,6 +27,38 @@ C.data = single(C.data); % remove high motion volumes, convert to single type;
 OutDir = fullfile(BaseDir, Subject, 'mshbm_output');
 mkdir(OutDir);
 
+%% PFM with MS-HBM:
+
 PriorWeight = 1; % this controls how much weight the spatial priors impose
 Smoothness = 10; % controls how likely neighboring vertices are to belong to the same network
 pfm_mshbm(C, MidthickSurfs, OutDir, PriorWeight, Smoothness, Params, Paths);
+
+%% Calculate the network sizes:
+
+D = ft_read_cifti_mod('/home/hanwang/Documents/Data/ucl/gos_ich/hcp_example_data/100307/mshbm_output/MS-HBM_FunctionalNetworks_VertexWiseThresh0.01_w1_c10.dlabel.nii');
+VA= ft_read_cifti_mod('/home/hanwang/Documents/Data/ucl/gos_ich/hcp_example_data/100307/mshbm_output/MS-HBM_FunctionalNetworks_VertexWiseThresh0.01_w1_c10.dtseries.nii');
+Structures = {'CORTEX_LEFT','CORTEX_RIGHT'}; % in this case, cortex only.
+NetworkSize = pfm_calculate_network_size(D,VA,Structures);
+
+close all; % blank slate
+H = figure; % prellocate parent figure
+set(H,'position',[1 1 325 500]); hold;
+
+
+% unique functional networks;
+uCi = unique(nonzeros(D.data));
+
+
+for i = 1:length(uCi)
+    Tmp = nan(1,length(Params.NetworkLabels));
+    Tmp(i) = NetworkSize(i);
+    barh(Tmp,'FaceColor',Params.NetworkColors(i,:));
+    text((NetworkSize(i)+0.1),i,[num2str(NetworkSize(i),3) '%']);
+end
+
+% make it pretty;
+yticklabels(Params.NetworkLabels); 
+yticks(1:length(uCi)); ylim([0 21]);
+xlim([0 25]); xticks(0:5:25);
+set(gca,'fontname','arial','fontsize',10,'TickLength',[0 0],'TickLabelInterpreter','none');
+xlabel('% of Cortical Surface');
